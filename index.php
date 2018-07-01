80ed2a4f727409753b267bb6e08018c865092f9e
techwebteam <soporte@techwebgt.com>
2018-07-01 13:15:50 -0600

    Estructura semántica de la cabecera de index

---
+++ index.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Viral</title>
+  <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css">
+
+
+</head>
+<body>
+
+  <header class="row">
+    <section class="col-6 header__main">
+      <section class="col-12 header__logo">
+        <figure>
+          <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/viral.png" alt="logo viral">
+        </figure>
+      </section>
+      <nav class="col-12 header__nav">
+        <ul class="nav nav__main">
+          <li class="nav-item nav__item">
+            <a class="nav-link nav__link active" href="#">Tecnología</a>
+          </li>
+          <li class="nav-item nav__item">
+            <a class="nav-link nav__link" href="#">Youtube</a>
+          </li>
+          <li class="nav-item nav__item">
+            <a class="nav-link nav__link" href="#">Música</a>
+          </li>
+          <li class="nav-item nav__item">
+            <a class="nav-link nav__link" href="#">Cine</a>
+          </li>
+          <li class="nav-item nav__item">
+            <a class="nav-link nav__link" href="#">Series</a>
+          </li>
+        </ul>
+      </nav>
+    </section>
+    <section class="col-6 header__sec">
+      <div class="row">
+        <nav class="col-5 nav__social">
+          <ul class="nav nav__main">
+            <li class="nav-item nav__item">
+              <a class="nav-link nav__link active" href="#">
+                <i class="fab fa-facebook-square"></i>
+              </a>
+            </li>
+            <li class="nav-item nav__item">
+              <a class="nav-link nav__link" href="#">
+                <i class="fab fa-twitter-square"></i>
+              </a>
+            </li>
+            <li class="nav-item nav__item">
+              <a class="nav-link nav__link" href="#">
+                <i class="fab fa-instagram"></i>
+              </a>
+            </li>
+            <li class="nav-item nav__item">
+              <a class="nav-link nav__link" href="#">
+                <i class="fab fa-youtube"></i>
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <section class="search__input col-7">
+          <div class="input-group mb-3">
+            <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2">
+            <div class="input-group-append">
+              <button class="btn btn-outline-secondary" type="button"><i class="fas fa-search"></i></button>
+            </div>
+          </div>
+        </section>
+      </div>
+      
+      
+    </section>
+  </header>
+
+</body>
+</html><!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Viral</title>
+  <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css">
+
+
+</head>
+<body>
+
+  <header class="row">
+    <section class="col-6 header__main">
+      <section class="col-12 header__logo">
+        <figure>
+          <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/viral.png" alt="logo viral">
+        </figure>
+      </section>
+      <nav class="col-12 header__nav">
+        <ul class="nav nav__main">
+          <li class="nav-item nav__item">
+            <a class="nav-link nav__link active" href="#">Tecnología</a>
+          </li>
+          <li class="nav-item nav__item">
+            <a class="nav-link nav__link" href="#">Youtube</a>
+          </li>
+          <li class="nav-item nav__item">
+            <a class="nav-link nav__link" href="#">Música</a>
+          </li>
+          <li class="nav-item nav__item">
+            <a class="nav-link nav__link" href="#">Cine</a>
+          </li>
+          <li class="nav-item nav__item">
+            <a class="nav-link nav__link" href="#">Series</a>
+          </li>
+        </ul>
+      </nav>
+    </section>
+    <section class="col-6 header__sec">
+      <div class="row">
+        <nav class="col-5 nav__social">
+          <ul class="nav nav__main">
+            <li class="nav-item nav__item">
+              <a class="nav-link nav__link active" href="#">
+                <i class="fab fa-facebook-square"></i>
+              </a>
+            </li>
+            <li class="nav-item nav__item">
+              <a class="nav-link nav__link" href="#">
+                <i class="fab fa-twitter-square"></i>
+              </a>
+            </li>
+            <li class="nav-item nav__item">
+              <a class="nav-link nav__link" href="#">
+                <i class="fab fa-instagram"></i>
+              </a>
+            </li>
+            <li class="nav-item nav__item">
+              <a class="nav-link nav__link" href="#">
+                <i class="fab fa-youtube"></i>
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <section class="search__input col-7">
+          <div class="input-group mb-3">
+            <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2">
+            <div class="input-group-append">
+              <button class="btn btn-outline-secondary" type="button"><i class="fas fa-search"></i></button>
+            </div>
+          </div>
+        </section>
+      </div>
+      
+      
+    </section>
+  </header>
+
+</body>
+</html>
